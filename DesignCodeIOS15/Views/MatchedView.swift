c3a5334d1ecf97955891d3ccac9cf5240741de59
wiz-boc <wiz_kidz@outlook.com>
2023-03-12 09:31:09 -0500

    Work on sample view for match geo

--- DesignCodeIOS15/Views/MatchedView.swift
+++ DesignCodeIOS15/Views/MatchedView.swift
@@ -13,46 +13,112 @@ struct MatchedView: View {
     var body: some View {
         ZStack {
             if !show {
-                VStack(alignment: .leading, spacing: 12) {
-                    Text("SwiftUI")
-                        .font(.largeTitle.weight(.bold))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Text("20 sections - 3 hours".uppercased())
-                        .font(.footnote.weight(.semibold))
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    
-                    Text("Build an iOS app for iOS 15 with custom layputs, animation and ..")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "text", in: namespace)
+                VStack {
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 12){
+                        Text("SwiftUI")
+                            .font(.largeTitle.weight(.bold))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Text("20 sections - 3 hours".uppercased())
+                            .font(.footnote.weight(.semibold))
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        
+                        Text("Build an iOS app for iOS 15 with custom layputs, animation and ..")
+                            .font(.footnote)
+                            .matchedGeometryEffect(id: "text", in: namespace)
+                    }
+                    .padding(20)
+                    .background(
+                        Rectangle()
+                            .fill(.ultraThinMaterial)
+                            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                            .matchedGeometryEffect(id: "blue", in: namespace)
+                    )
                 }
-                .padding(20)
                 .foregroundStyle(.white)
                 .background(
-                    Color.red.matchedGeometryEffect(id: "background", in: namespace)
+                    Image("Illustration 9")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .matchedGeometryEffect(id: "image", in: namespace)
                 )
+                .background(
+                    Image("Background 5")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "background", in: namespace)
+                )
+                .mask {
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
+                }
+                .frame(height: 300)
                 .padding(20)
                 
             }else{
-                VStack(alignment: .leading, spacing: 12) {
-                    Spacer()
-                    Text("Build an iOS app for iOS 15 with custom layputs, animation and ..")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "text", in: namespace)
-                    Text("20 sections - 3 hours".uppercased())
-                        .font(.footnote.weight(.semibold))
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    Text("SwiftUI")
-                        .font(.largeTitle.weight(.bold))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                ScrollView {
+                    VStack{
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 500)
+                    .foregroundStyle(.black)
+                    .background(
+                        Image("Illustration 9")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .matchedGeometryEffect(id: "image", in: namespace)
+                    )
+                    .background(
+                        Image("Background 5")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .matchedGeometryEffect(id: "background", in: namespace)
+                    )
+                    .mask {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .matchedGeometryEffect(id: "mask", in: namespace)
                 }
-                .padding(20)
-                .foregroundStyle(.white)
-                .background(
-                    Color.blue.matchedGeometryEffect(id: "background", in: namespace)
-                )
+                    .overlay(content: {
+                        VStack(alignment: .leading, spacing: 12) {
+                            Text("Build an iOS app for iOS 15 with custom layputs, animation and ..")
+                                .font(.footnote)
+                                .matchedGeometryEffect(id: "text", in: namespace)
+                            Text("20 sections - 3 hours".uppercased())
+                                .font(.footnote.weight(.semibold))
+                                .matchedGeometryEffect(id: "subtitle", in: namespace)
+                            Text("SwiftUI")
+                                .font(.largeTitle.weight(.bold))
+                                .matchedGeometryEffect(id: "title", in: namespace)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            Divider()
+                            
+                            HStack{
+                                Image("Avatar Default")
+                                    .resizable()
+                                    .frame(width: 26, height: 26)
+                                    .cornerRadius(10)
+                                    .padding(8)
+                                    .background(.ultraThinMaterial, in :
+                                                    RoundedRectangle(cornerRadius: 18, style: .continuous))
+                                    .strokeStyle(cornerRadius: 18)
+                                Text("Taught by Meng To")
+                            }
+                        }
+                        .padding(20)
+                        .background(
+                            Rectangle()
+                                .fill(.ultraThinMaterial)
+                                .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                                .matchedGeometryEffect(id: "blue", in: namespace)
+                        )
+                        .offset(y: 200)
+                        .padding(20)
+                    })
+                }
             }
         }
         .onTapGesture {
