09d2c6845760b2b5e949401443ffdfd755c4779a
wiz-boc <wiz_kidz@outlook.com>
2023-03-29 07:02:28 -0500

    create textfield

--- DesignCodeIOS15/Views/SignUpView.swift
+++ DesignCodeIOS15/Views/SignUpView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    enum Field: Hashable {
+        case email
+        case password
+    }
+    
+    @State var email = ""
+    @State var password = ""
+    @FocusState var focusedField: Field?
+    @State var circleY: CGFloat = 120
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 6) {
             Text("Hello, World!")
                 .font(.largeTitle).bold()
             Text("Access 120+ hours of courses, tutorials and livestreams")
                 .font(.headline)
+            
+            TextField("Email", text: $email)
+                .inputStyle(icon: "mail")
+                .textContentType(.emailAddress)
+                .keyboardType(.emailAddress)
+                .disableAutocorrection(true)
+                .focused($focusedField, equals: .email)
+                .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear ,radius: 10, x: 0, y: 3)
+            
+            SecureField("Password", text:$password)
+                .inputStyle(icon: "lock")
+                .textContentType(.password)
+                .focused($focusedField, equals: .password)
+                .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear ,radius: 10, x: 0, y: 3)
+                
+            
             Button {} label: {
                 Text("Create an account")
                     .frame(maxWidth: .infinity)
@@ -45,12 +72,28 @@ struct SignUpView: View {
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .background(
+            Circle().fill(.blue)
+                .frame(width: 68, height: 68)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .offset(y: circleY)
+        )
         .strokeStyle(cornerRadius: 30)
         .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
         .padding(20)
         .background(
             Image("Blob 1").offset(x: 200, y: -100)
         )
+        .onChange(of: focusedField) { value in
+            withAnimation {
+                if value == .email {
+                    circleY = 120
+                } else {
+                    circleY = 190
+                }
+            }
+            
+        }
     }
 }
 
